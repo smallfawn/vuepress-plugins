87e6d587f40cf027707160a3cafcaa8de44d4483
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-10-01 10:33:44 +0800

    1

--- vuepress-plugin-hitokoto/dist/client.js
+++ vuepress-plugin-hitokoto/dist/client.js
@@ -1,11 +1,11 @@
 import { defineClientConfig } from '@vuepress/client';
-//import { onMounted, createApp } from 'vue';
+import { h } from 'vue';
 import hitokoto from './app.vue';
 
 const type = _type
 
 export default defineClientConfig({
-    enhance({app}) {
+    enhance({ app }) {
         app.component('hitokoto', {
             // 通过 props 传递参数
             render() {
