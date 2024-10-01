c099cfa8886783d686bcf58362e9a936d0ac21c7
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-10-01 10:52:04 +0800

    1

--- vuepress-plugin-music/dist/client.js
+++ vuepress-plugin-music/dist/client.js
@@ -12,7 +12,7 @@ export default defineClientConfig({
       const body = document.querySelector('body');
       const div = document.createElement('div');
       div.style.position = 'fixed'; // 固定定位
-      div.style.right = '20px'; // 距离右侧20像素
+      div.style.left = '20px'; // 距离右侧20像素
       div.style.bottom = '20px'; // 距离底部20像素
       div.style.zIndex = '1000'; // 确保在最上层
       const musicApp = createApp(music, { src, title, artist });
