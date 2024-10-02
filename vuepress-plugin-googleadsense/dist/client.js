5ff1cfa7e46f8ff4e3fdbf4552449237d133f3d3
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-10-02 15:31:29 +0800

    1

--- vuepress-plugin-googleadsense/dist/client.js
+++ vuepress-plugin-googleadsense/dist/client.js
@@ -1,17 +1,15 @@
 import { defineClientConfig } from '@vuepress/client';
+const adsid = __googleAdSense_adsid
 export default defineClientConfig({
     enhance() {
         if (typeof window !== 'undefined' && typeof document !== 'undefined') {
-            if (__VUEPRESS_SSR__ || !__googleAdSense_adsid)
+            if (!__googleAdSense_adsid)
                 return;
-            if (window.adsbygoogle) {
-                return;
-            }
             const adsScript = document.createElement('script');
-            adsScript.setAttribute('data-ad-client', __googleAdSense_adsid);
             adsScript.async = true;
+            adsScript.setAttribute("crossorigin", "anonymous");
             adsScript.src =
-                'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+                `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adsid}`;
             document.head.appendChild(adsScript);
         }
     },
--- vuepress-plugin-googleadsense/dist/index.js
+++ vuepress-plugin-googleadsense/dist/index.js
@@ -1,6 +1,6 @@
 import { path, getDirname } from '@vuepress/utils';
 const __dirname = getDirname(import.meta.url);
-export const googleAdSensePlugin = ({ id, }) => ({
+export const googleAdSensePlugin = ({ id }) => ({
     name: 'vuepress-plugin-googleadsense',
     clientConfigFile: path.resolve(__dirname, './client.js'),
     define: {
