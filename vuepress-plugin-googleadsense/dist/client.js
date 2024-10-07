15454caaa8c1e21a2489861eb4080dfa664fd1b2
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-10-07 13:21:49 +0800

    1

--- vuepress-plugin-googleadsense/dist/client.js
+++ vuepress-plugin-googleadsense/dist/client.js
@@ -2,29 +2,31 @@ import { defineClientConfig } from '@vuepress/client';
 
 // 全局对象扩展定义
 if (typeof window !== 'undefined') {
-    window.adsbygoogle = window.adsbygoogle || { loaded: false, push: function () { } };
+  window.adsbygoogle = window.adsbygoogle || { loaded: false, push: function() {} };
 }
 
-// 定义开发和 SSR 变量
-const __VUEPRESS_DEV__ = typeof __VUEPRESS_DEV__ !== 'undefined' ? __VUEPRESS_DEV__ : false;
-const __VUEPRESS_SSR__ = typeof __VUEPRESS_SSR__ !== 'undefined' ? __VUEPRESS_SSR__ : false;
-const ADS_ID = typeof ADS_ID !== 'undefined' ? ADS_ID : '__smallfawn_googleAdSense_adsid';
+// 检查全局变量是否已经被定义，防止初始化前访问
+const isSSR = typeof __VUEPRESS_SSR__ !== 'undefined' ? __VUEPRESS_SSR__ : false;
+const adsId = typeof __smallfawn_googleAdSense_adsid !== 'undefined' ? __smallfawn_googleAdSense_adsid : '';
 
 export default defineClientConfig({
-    enhance() {
-        // 如果是 SSR 或者没有 ADS_ID，直接返回
-        if (__VUEPRESS_SSR__ || !ADS_ID) return;
+  enhance() {
+    // 如果是 SSR 或者没有 ADS_ID，直接返回
+    if (isSSR || !adsId) return;
 
-        // 避免重复加载广告脚本
-        if (window.adsbygoogle && window.adsbygoogle.loaded) {
-            return;
-        }
-        // 创建广告 script 标签
-        const adsScript = document.createElement('script');
-        adsScript.setAttribute('data-ad-client', ADS_ID);
-        adsScript.async = true;
-        adsScript.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
-        // 插入 script 到 head 中
-        document.head.appendChild(adsScript);
-    },
+    // 避免重复加载广告脚本
+    if (window.adsbygoogle && window.adsbygoogle.loaded) {
+      return;
+    }
+
+    // 创建广告 script 标签
+    const adsScript = document.createElement('script');
+    adsScript.setAttribute('data-ad-client', adsId);
+    adsScript.async = true;
+    adsScript.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+
+    // 插入 script 到 head 中
+    document.head.appendChild(adsScript);
+
+  }
 });
